74c8a0052cb81d91e40d3600425f6e36d6bdcd9b
marimuthu <mr.java007@gmail.com>
2010-06-25 17:26:42 +0530

    New Source

---
+++ src/com/Demo.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Demo {
+	
+	static {
+		System.out.println("static block initialized");
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(" I am execute after static block");
+	}
+
+}
