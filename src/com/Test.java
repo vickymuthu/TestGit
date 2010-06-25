36beae1cc76af13fbb9e9daca48b5cae4818a63e
marimuthu <mr.java007@gmail.com>
2010-06-25 17:15:50 +0530

    Comment removed

--- src/com/Test.java
+++ src/com/Test.java
@@ -3,7 +3,7 @@ package com;
 public class Test {
 	public static void main(String args[]) {
 		System.out.println("hi");
-		//System.out.println("I am committed");
+		System.out.println("I am committed");
 	}
 
 }
